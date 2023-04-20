ab579ef638394145e136635469076d5021e9cf99
Anirudh A V <anirudh.av02@gmail.com>
2023-04-20 20:30:12 +0530

    feat : Map successfully renders on page Marker of the user's current location is rendered on the map

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5pcnVkaGF2MDIiLCJhIjoiY2xncDR0cGI1MGJubDNyc3UwcHhhd3BsayJ9.mqNU3ZXIGHnS5PifEDrUtQ';
 
 function App() {
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const marker = useRef(null);
+  
   const [longitude, setLongitude] = useState(0)
   const [latitude, setLatitude] = useState(0)
+  const [zoom, setZoom] = useState(12)
 
   useEffect(() => {
 
@@ -21,9 +29,20 @@ function App() {
     } else {
       navigator.geolocation.getCurrentPosition(success, error)
     }
-
-      
   }, []);
+
+  useEffect(() => {
+    if (longitude === 0 && latitude === 0) return; // wait for location to be retrieved
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [longitude, latitude],
+      zoom: zoom
+    });
+    marker.current = new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map.current);
+  }, [longitude, latitude, zoom]);
+
+
 
   const triggerEmergency = () => {
     console.log('EMERGENCY')
@@ -34,6 +53,16 @@ function App() {
       <button onClick={triggerEmergency}>Emergency</button>
       <p>Longitude: {longitude}</p>
       <p>Latitude: {latitude}</p>
+      <div>
+        <div className="sidebar">
+          Longitude: {longitude} | Latitude: {latitude} | Zoom: {zoom}
+        </div>
+        <div ref={mapContainer} className="map-container" 
+        onScroll={() => {
+          console.log('scrolling')
+        }}
+        />
+      </div>
     </div>
   )
 }
