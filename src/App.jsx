b94bd666b400731f5d28812533b2565a933328db
Anirudh A V <anirudh.av02@gmail.com>
2023-04-22 23:22:42 +0530

    feat: Directions to the nearest hospital added
    Now the user can get directions to the nearest hospital from the current location.
    List of hospitals is stored in a separate file.
    List of hospitals are displayed in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState, useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
 
-import marker_svg from'./Assets/marker.svg';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
+
+import marker_svg from './Assets/marker.svg';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYW5pcnVkaGF2MDIiLCJhIjoiY2xncDR0cGI1MGJubDNyc3UwcHhhd3BsayJ9.mqNU3ZXIGHnS5PifEDrUtQ';
 
@@ -14,6 +18,9 @@ function App() {
   const [zoom, setZoom] = useState(15);
   const [hospitals, setHospitals] = useState([]);
   const [hospitalData, setHospitalData] = useState(null);
+  const [selectedHospital, setSelectedHospital] = useState(null);
+  const [map, setMap] = useState(null);
+
 
   useEffect(() => {
 
@@ -41,14 +48,48 @@ function App() {
       center: [longitude, latitude],
       zoom: zoom
     });
-  
+    setMap(map);
+
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+      showAccuracyCircle: true,
+      showUserLocation: true,
+      fitBoundsOptions: {
+        maxZoom: 15
+      },
+    }), 'bottom-right');
+
+    const nav = new mapboxgl.NavigationControl();
+    // map.addControl(nav, 'top-left');
+
+    const scale = new mapboxgl.ScaleControl({
+      maxWidth: 80,
+      unit: 'imperial'
+    });
+    map.addControl(scale);
+
+    scale.setUnit('metric');
+
     const geocoder = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl,
     });
-  
+
     map.addControl(geocoder);
-  
+
+    const Directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: 'metric',
+      profile: 'mapbox/driving',
+      controls: { instructions: false },
+    });
+
+    map.addControl(Directions, 'top-left');
+
     map.on('load', () => {
       // Use Mapbox Places API to find nearby hospitals
       fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/hospital.json?proximity=${longitude},${latitude}&access_token=${mapboxgl.accessToken}&limit=10`)
@@ -58,15 +99,24 @@ function App() {
           setHospitals(res.features);
           console.log(res.features)
 
-          // const image = new Image(15, 15);
-          // image.src = marker_svg;
-          // map.addImage('hospital-15', image);
-
+          console.log('Adding markers to map')
           hospitals.forEach((hospital) => {
-            const marker = new mapboxgl.Marker({ color: '#FF0000' }).setLngLat(hospital.geometry.coordinates).addTo(map);
+            const marker = new mapboxgl.Marker({ color: '#FF0000' })
+              .setLngLat(hospital.geometry.coordinates)
+              .addTo(map);
+            console.log('Adding event listeners to markers')
+
+            marker.getElement().addEventListener('mouseenter', () => {
+              marker.getElement().style.cursor = 'pointer';
+            });
+
+            marker.getElement().addEventListener('click', () => {
+              setSelectedHospital(hospital);
+            });
+
           });
-  
-          // Center map to the extent of the hospital markers
+
+          console.log('Fitting map to bounds')
           const bounds = new mapboxgl.LngLatBounds();
           hospitals.forEach((feature) => {
             bounds.extend(feature.geometry.coordinates);
@@ -75,15 +125,66 @@ function App() {
         })
         .catch((err) => console.log(err));
     });
-  
+
     marker.current = new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
-  
-  }, [longitude, latitude, zoom]);
-  
+
+  }, []);
+
+
+  useEffect(() => {
+    if (!selectedHospital) return; // no hospital selected
+
+    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${longitude},${latitude};${selectedHospital.center[0]},${selectedHospital.center[1]}?access_token=${mapboxgl.accessToken}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.routes[0].geometry);
+        console.log('Creating new directions control')
+
+        
+        const directions = new MapboxDirections({
+          accessToken: mapboxgl.accessToken,
+          unit: 'metric',
+          profile: 'mapbox/driving',
+          controls: { instructions: false },
+        });
+
+        console.log('Adding route to map')
+        console.log(data.routes[0].geometry)
+        directions.setOrigin([longitude, latitude]);
+        directions.setDestination([selectedHospital.center[0], selectedHospital.center[1]]);
+        directions.addRoute(data.routes[0].geometry);
+        map.addControl(directions, 'top-left');
+        map.fitBounds(directions.getBounds(), { padding: 20 });
+      })
+      .catch((err) => console.log(err));
+
+    
+    return () => {
+      if (map.getSource('route')) {
+        map.removeLayer('route');
+        map.removeSource('route');
+        map.removeControl(directions);
+      }
+    };
+  }, [selectedHospital]);
+
+
 
   return (
     <div>
       <div ref={mapContainer} className="map-container" />
+      {hospitals && hospitals.length > 0 && (
+        <div className='Hospital-List'>
+        {/* <h2>Hospitals</h2> */}
+        <div>
+          {hospitals.map((hospital) => (
+            <div key={hospital.id} onClick={() => setSelectedHospital(hospital)}>
+              <p>{hospital.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      )}
     </div>
   );
 }
---
+++ src/Data/location.js
@@ -0,0 +1,142 @@
+export const hospitals = [
+    {
+        "id": 1,
+        "name": "Thiruvananthapuram Medical College Casualty",
+        "coordinates": "8.5235626173231, 76.92779843082823"
+    },
+    {
+        "id": 2,
+        "name": "Providence Endocrine & Diabetes Specialty Centre",
+        "coordinates": "8.531637201099311, 76.92073445398033"
+    },
+    {
+        "id": 3,
+        "name": "Arthro care",
+        "coordinates": "8.553169386631982, 76.9168770241366"
+    },
+    {
+        "id": 4,
+        "name": "AJ Hospital",
+        "coordinates": "8.568460264171739, 76.8728721511262"
+    },
+    {
+        "id": 5,
+        "name": "PHC Ulloor",
+        "coordinates": "8.545689695701508, 76.90785151064188"
+    },
+    {
+        "id": 6,
+        "name": "KJK Hospital and Fertility Research Centre ",
+        "coordinates": "8.541649062002989, 76.93930896646637"
+    },
+    {
+        "id": 7,
+        "name": "IMB Hospital",
+        "coordinates": "8.557553072705618, 76.91600709530162"
+    },
+    {
+        "id": 8,
+        "name": "Chest Diseases Hospital",
+        "coordinates": "8.519957681977754, 76.91425592546955"
+    },
+    {
+        "id": 9,
+        "name": "Station Medicare Centre, S.M.C",
+        "coordinates": "8.518760460117843, 76.9111570953011"
+    },
+    {
+        "id": 10,
+        "name": "KIMS Hospital",
+        "coordinates": "8.514124483422494, 76.90953551906179"
+    },
+    {
+        "id": 11,
+        "name": "Anugraham Neurocare",
+        "coordinates": "8.515155159821315, 76.93520226646604"
+    },
+    {
+        "id": 12,
+        "name": "Janata Clinic",
+        "coordinates": "8.543145147473512, 76.88653251709788"
+    },
+    {
+        "id": 13,
+        "name": "Cosmopolitan Hospital",
+        "coordinates": "8.515576927451734, 76.9355663404844"
+    },
+    {
+        "id": 14,
+        "name": "Bethesda Medi Clinic",
+        "coordinates": "8.58216156415073, 76.92206527844552"
+    },
+    {
+        "id": 15,
+        "name": "Sree Avittam Thirunal Hospital (SAT)",
+        "coordinates": "8.522899328238896, 76.92476702699017"
+    },
+    {
+        "id": 16,
+        "name": "Jubilee Memorial Hospital",
+        "coordinates": "8.501983847806349, 76.95231716646585"
+    },
+    {
+        "id": 17,
+        "name": "Meditrina Hospital",
+        "coordinates": "8.515435049284127, 76.94430206646601"
+    },
+    {
+        "id": 18,
+        "name": "T.M. Medical Centre",
+        "coordinates": "8.540779444522087, 76.88017924613114"
+    },
+    {
+        "id": 19,
+        "name": "Indian Institute of Diabetes",
+        "coordinates": "8.520277754180333, 76.9115433333646"
+    },
+    {
+        "id": 20,
+        "name": "SCT Institute for Medical Sciences & Technology",
+        "coordinates": "8.521135596326664, 76.9263969602194"
+    },
+    {
+        "id": 21,
+        "name": "Pangappara PHC",
+        "coordinates": "8.557487731966711, 76.90342844048767"
+    },
+    {
+        "id": 22,
+        "name": "GG Hospital",
+        "coordinates": "8.515784145453573, 76.9332936597226"
+    },
+    {
+        "id": 23,
+        "name": "Lords Hospital",
+        "coordinates": "8.507128807594025, 76.91072526952098"
+    },
+    {
+        "id": 24,
+        "name": "Credence Hospital",
+        "coordinates": "8.528894974805247, 76.93121345398117"
+    },
+    {
+        "id": 25,
+        "name": "Medcare Health Centre",
+        "coordinates": "8.5473164527959, 76.91687238180678"
+    },
+    {
+        "id": 26,
+        "name": "TSC Hospital Kazhakootam",
+        "coordinates": "8.539927478257573, 76.8785269221894"
+    },
+    {
+        "id": 27,
+        "name": "CSI Mission Hospital",
+        "coordinates": "8.574595515551112, 76.86857053380736"
+    },
+    {
+        "id": 28,
+        "name": "General Hospital ",
+        "coordinates": "8.499618417550836, 76.9429911859288"
+    }
+]
